0e2b9cdb5ecc78e6c808724d31aaf61756907b49
huangxiaojing <huangxiaojingcn@gmail.com>
2019-01-07 11:44:12 +0800

    Feat: 添加 antd

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+
+export default class Counter extends Component {
+  render() {
+    return(
+        <div>计数器 demo
+
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
